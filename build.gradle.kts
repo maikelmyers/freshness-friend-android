f2deb556ec31472393667a7d11738da4eb48ad5e
micha <micha@example.com>
2026-05-10 13:06:03 +0200

    Remove Google Services plugin - using Supabase instead

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
     kotlin("android") version "1.9.22" apply false
 }
 
+subplugins {
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
